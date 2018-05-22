b7937bae5e36d8a73297b03f246339845ebc5b93
Irinej Stjepanović <irinejs@gmail.com>
2018-05-22 22:36:20 +0200

    Setting up Mapper and CategoryService

---
+++ src/main/java/com/api/v1/mapper/CategoryMapper.java
@@ -0,0 +1,16 @@
+package com.api.v1.mapper;
+
+import com.api.v1.model.CategoryDTO;
+import com.domain.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CategoryMapper {
+
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+    @Mapping(source = "id", target = "id")
+    CategoryDTO categoryToCategoryDTO(Category category);
+}
---
+++ src/main/java/com/services/CategoryService.java
@@ -0,0 +1,12 @@
+package com.services;
+
+import com.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
